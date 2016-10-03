9779012f0f1005c39db10276d2a677fc441b7ea6
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2016-10-04 00:28:51 +0200

    Correct answers and scoring

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
 let shelves = [
-    { name: "Neogene", answer: "6245" },
-    { name: "Paleogene", answer: "2359" }
+    { name: "Zenozoic", answer: 0 },
+    { name: "Neogene", answer: 1 },
+    { name: "Paleogene", answer: 2 },
+    { name: "Mesozoic", answer: 3 },
+    { name: "Cretaceous", answer: 4 },
+    { name: "Jurassic", answer: 5 },
+    { name: "Triassic", answer: 6 },
+    { name: "Paleozoic", answer: 7 },
+    { name: "Permian", answer: 8 },
+    { name: "Carboniferous", answer: 9 },
+    { name: "Devonian", answer: 10 },
+    { name: "Silurian", answer: 11 },
+    { name: "Ordovician", answer: 12 },
+    { name: "Cambrian", answer: 13 },
 ];
 
-let SG = document.querySelector("#Shelfgroup");
-let cells = document.querySelectorAll('#Chronoshelves td');
+let SG = document.getElementById('Shelfgroup');
+let CS = document.getElementById('Chronoshelves');
+let minleft = CS.getBoundingClientRect().right + 10;
+let cells = CS.querySelectorAll('td');
 let dragged = -1;
 let drop = -1;
+let totalCorrect = 0;
+
+function addClass (el, cname) {
+    if (el.className.indexOf(cname)<0) {
+        el.className += ' '+cname;
+    }
+}
+
+function removeClass (el, cname) {
+    el.className = el.className.replace(' '+cname, '');
+}
 
 for (let i=0; i<shelves.length; i++) {
     let shelf = shelves[i],
@@ -23,7 +48,7 @@ for (let i=0; i<shelves.length; i++) {
     };
     let startdrag = (e) => {
         dragged = i;
-        s.className = 'ondrag';
+        addClass(s, 'ondrag');
         document.addEventListener('mouseup', enddrag, true);
         document.addEventListener('mousemove', drag, false);
     };
@@ -38,11 +63,25 @@ for (let i=0; i<shelves.length; i++) {
                 s.style.left = rect.left + 'px';
                 s.style.top = rect.top + 'px';
             }
+            if (drop == shelf.answer) {
+                shelf.correct = true;
+                removeClass(s, 'wrong');
+            } else {
+                shelf.correct = false;
+                addClass(s, 'wrong');
+            }
+        } else {
+            removeClass(s, 'wrong');
+            if (s.offsetLeft < minleft) {
+                s.style.left = (s.offsetLeft+minleft-s.offsetLeft)+'px';
+            }
         }
         dragged = -1;
-        s.className = '';
+        removeClass(s, 'ondrag');
         document.removeEventListener('mousemove', drag, false);
         document.removeEventListener('mouseup', enddrag, true);
+        totalCorrect = shelves.reduce((sum, x) => sum+(x.correct?1:0), 0);
+        console.log(totalCorrect);
     };
     s.addEventListener('mousedown', startdrag, true);
 }
@@ -50,22 +89,21 @@ for (let i=0; i<shelves.length; i++) {
 for (let i=0; i<cells.length; i++) {
     let c = cells[i],
         vert = c.rowSpan>1,
-        myheight = c.clientHeight,
-        defw;
+        myheight = c.clientHeight;
+    c.dropped = -1;
     c.onmouseover = (e) => {
-        if (dragged < 0) { return; }
+        if (dragged < 0 || c.dropped >= 0) { return; }
         let d = shelves[dragged].dom;
         drop = i;
         if (vert) {
             d.style.transform = 'rotate(-90deg)';
-            defw = d.style.width;
             d.style.width = myheight+'px';
         }
     };
     c.onmouseout = (e) => {
-        if (dragged < 0) { return; }
-        let d = shelves[dragged].dom;
         drop = -1;
+        if (dragged < 0 || c.dropped >= 0) { return; }
+        let d = shelves[dragged].dom;
         if (vert) {
             d.style.transform = '';
             d.style.width = '';
